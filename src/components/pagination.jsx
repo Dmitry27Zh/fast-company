a79d4d88dbc7b86266bfe46147444ff7e5b545ce
Dmitry <dmitry27zh@gmail.com>
2023-08-25 22:17:23 +0300

    Add Pagination content

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 
+const PageItem = (props) => {
+  const { text } = props
+
+  return (
+    <li className="page-item">
+      <button className="page-link">{text}</button>
+    </li>
+  )
+}
+
+const Pages = (props) => {
+  const { pages } = props
+
+  return (
+    <ul className="pagination">
+      {pages.map((text, index) => (
+        <PageItem key={index} text={text} />
+      ))}
+    </ul>
+  )
+}
+
 const Pagination = (props) => {
   const { itemsCount, pageSize } = props
-  console.log(itemsCount)
-  console.log(pageSize)
-  return <div></div>
+  const pagesCount = Math.trunc(itemsCount / pageSize)
+  const pages = new Array(pagesCount + 2).fill().map((_, index, arr) => {
+    if (index === 0) {
+      return 'Previous'
+    }
+
+    if (index === arr.length - 1) {
+      return 'Next'
+    }
+
+    return index
+  })
+
+  return (
+    <nav aria-label="Page navigation">
+      <Pages pages={pages} />
+    </nav>
+  )
 }
 
 export default Pagination
