9550cdfab91ad83caeb574109b3833f5c79d2437
Dmitry <dmitry27zh@gmail.com>
2023-08-26 10:44:22 +0300

    Add PageItem disabled and active modifications

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,22 +1,49 @@
 import React from 'react'
 
 const PageItem = (props) => {
-  const { text } = props
+  const { text, disabled, active } = props
+  const getClassName = () => {
+    let result = 'page-item'
+    result += disabled ? ' disabled' : ''
+    result += active ? ' active' : ''
+
+    return result
+  }
 
   return (
-    <li className="page-item">
+    <li className={getClassName()}>
       <button className="page-link">{text}</button>
     </li>
   )
 }
 
 const Pages = (props) => {
-  const { pages } = props
+  const { pagesCount, currentPage } = props
+  let pages = Array.from({ length: pagesCount }, (_, index) => {
+    return {
+      text: index + 1,
+      disabled: false,
+      active: index + 1 === currentPage,
+    }
+  })
+  pages = [
+    {
+      text: 'Previous',
+      disabled: currentPage === 1,
+      active: false,
+    },
+    ...pages,
+    {
+      text: 'Next',
+      disabled: currentPage === pagesCount,
+      active: false,
+    },
+  ]
 
   return (
     <ul className="pagination">
-      {pages.map((text, index) => (
-        <PageItem key={index} text={text} />
+      {pages.map(({ text, disabled, active }, index) => (
+        <PageItem key={index} text={text} disabled={disabled} active={active} />
       ))}
     </ul>
   )
@@ -25,12 +52,11 @@ const Pages = (props) => {
 const Pagination = (props) => {
   const { itemsCount, pageSize } = props
   const pagesCount = Math.trunc(itemsCount / pageSize)
-  let pages = Array.from({ length: pagesCount }, (_, index) => index + 1)
-  pages = ['Previous', ...pages, 'Next']
+  let currentPage = 1
 
   return (
     <nav aria-label="Page navigation">
-      <Pages pages={pages} />
+      <Pages pagesCount={pagesCount} currentPage={currentPage} />
     </nav>
   )
 }
