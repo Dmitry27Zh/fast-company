4ff0ac00640342df1fc71477b0919acf20bb8458
Dmitry <dmitry27zh@gmail.com>
2023-09-03 12:54:17 +0300

    Start Login validation

--- src/layout/login.jsx
+++ src/layout/login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TextField from '../components/textField'
 
 const Login = () => {
@@ -6,14 +6,36 @@ const Login = () => {
         email: '',
         password: ''
     })
+    const [, setErrors] = useState()
     const { email, password } = data
+    const validate = () => {
+        const errors = {}
+
+        for (const fieldName in data) {
+            const fieldData = data[fieldName]
+
+            if (fieldData.trim().length === 0) {
+                errors[fieldName] = `${fieldName} is required`
+            }
+        }
+
+        setErrors(errors)
+        return Object.keys(errors).length === 0
+    }
+    useEffect(() => {
+        validate()
+    }, [data])
     const handleChange = (event) => {
         const { name, value } = event.target
         setData((previousState) => ({ ...previousState, [name]: value }))
     }
     const handleSubmit = (event) => {
         event.preventDefault()
-        console.log(data)
+        const isValid = validate()
+
+        if (isValid) {
+            console.log(data)
+        }
     }
 
     return (
