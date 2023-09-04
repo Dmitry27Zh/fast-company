d345f77f45efbfaf43d295bb7b3210e69c7d6d52
Dmitry <dmitry27zh@gmail.com>
2023-09-04 17:05:02 +0300

    Add TextField styles

--- src/components/textField.jsx
+++ src/components/textField.jsx
@@ -3,6 +3,12 @@ import PropTypes from 'prop-types'
 
 const TextField = (props) => {
     const { label, type, name, value, onChange, error } = props
+    const getInputClasses = () => {
+        let result = 'form-control'
+        result += error ? ' is-invalid' : ''
+
+        return result
+    }
     const renderError = () => {
         if (error) {
             return <p>{error}</p>
@@ -10,9 +16,10 @@ const TextField = (props) => {
     }
 
     return (
-        <div>
+        <div className='mb-4'>
             <label htmlFor={name}>{label}</label>
             <input
+                className={getInputClasses()}
                 type={type}
                 id={name}
                 value={value}
