c107ce4583046b432ec4f8ff11bf8b03a7d2c40a
Dmitry <dmitry27zh@gmail.com>
2023-09-04 18:25:09 +0300

    Toggle password show in TextField

--- src/components/textField.jsx
+++ src/components/textField.jsx
@@ -1,32 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const TextField = (props) => {
     const { label, type, name, value, onChange, error } = props
+    const [passwordShow, setPasswordShow] = useState(false)
     const getInputClasses = () => {
         let result = 'form-control'
         result += error ? ' is-invalid' : ''
 
         return result
     }
+    const togglePasswordShow = () => {
+        setPasswordShow((previousState) => !previousState)
+    }
     const renderError = () => {
         if (error) {
-            return <p>{error}</p>
+            return <div className='invalid-feedback'>{error}</div>
         }
     }
+    const renderPasswordVisibilityBtn = () => {
+        if (type === 'password') {
+            const getIconClasses = () => {
+                let result = 'bi'
+                result += passwordShow ? ' bi-eye-slash' : ' bi-eye'
+
+                return result
+            }
+
+            return <button className='btn btn-outline-secondary' type='button' onClick={togglePasswordShow}><i className={getIconClasses()}></i></button>
+        }
+    }
+
+    const renderType = type === 'password' && passwordShow ? 'text' : type
 
     return (
         <div className='mb-4'>
             <label htmlFor={name}>{label}</label>
-            <input
-                className={getInputClasses()}
-                type={type}
-                id={name}
-                value={value}
-                name={name}
-                onChange={onChange}
-            />
-            {renderError()}
+            <div className="input-group has-validation">
+                <input
+                    className={getInputClasses()}
+                    type={renderType}
+                    id={name}
+                    value={value}
+                    name={name}
+                    onChange={onChange}
+                />
+                {renderPasswordVisibilityBtn()}
+                {renderError()}
+            </div>
         </div>
     )
 }
