bfbcdb26a903c34a6fa97d0ddc35ffaf5aacd08a
Dmitry <dmitry27zh@gmail.com>
2023-09-04 19:01:50 +0300

    Debounce Search change

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import _ from 'lodash'
 
 const Search = (props) => {
     const { onSearch } = props
+    const handleChange = _.debounce((event) => {
+        onSearch(event)
+    }, 500)
 
-    return <div><input className='w-100 p-2' type='search' placeholder='...search' onChange={onSearch}/></div>
+    return <div><input className='w-100 p-2' type='search' placeholder='...search' onChange={handleChange}/></div>
 }
 
 Search.propTypes = {
