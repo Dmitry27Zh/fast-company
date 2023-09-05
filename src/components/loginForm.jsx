079dcbb9fdeaa8b19b91c8f9733178d34fddf33c
Dmitry <dmitry27zh@gmail.com>
2023-09-05 12:06:42 +0300

    Create LoginForm

---
+++ src/components/loginForm.jsx
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react'
+import TextField from '../components/textField'
+import { validator, getErrorMessageAtLeast } from '../utils/validator'
+import { isObjEmpty } from '../utils/object'
+
+const ValidationValue = {
+    minLength: 6,
+    maxLength: 18
+}
+
+const LoginForm = () => {
+    const [data, setData] = useState({
+        email: '',
+        password: ''
+    })
+    const [errors, setErrors] = useState({})
+    const { email, password } = data
+    useEffect(() => {
+        validate()
+    }, [data])
+    const validatorConfig = {
+        email: {
+            isRequired: {
+                message: 'Email is required'
+            },
+            isEmail: {
+                message: 'Incorrect email'
+            }
+        },
+        password: {
+            isRequired: {
+                message: 'Password is required'
+            },
+            containUpperCaseLetter: {
+                message: 'Password should contain one uppercase letter at least'
+            },
+            containDigit: {
+                message: 'Password should contain one digit at least'
+            },
+            minLength: {
+                value: ValidationValue.minLength,
+                message: `Password should contain minimum ${ValidationValue.minLength} symbols`
+            },
+            maxLength: {
+                value: ValidationValue.maxLength,
+                message: `Password should contain maximum ${ValidationValue.maxLength} symbols`
+            }
+        }
+    }
+    const isValid = (errors) => isObjEmpty(errors)
+    const validate = () => {
+        const errors = validator(data, validatorConfig)
+        setErrors(errors)
+
+        return isValid(errors)
+    }
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setData((previousState) => ({ ...previousState, [name]: value }))
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const isValid = validate()
+
+        if (isValid) {
+            console.log(data)
+        }
+    }
+
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 shadow p-4">
+                    <h3 className='mb-4'>Login</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            label="Email"
+                            name="email"
+                            value={email}
+                            onChange={handleChange}
+                            error={getErrorMessageAtLeast(errors.email)}
+                        />
+                        <TextField
+                            type="password"
+                            label="Password"
+                            name="password"
+                            value={password}
+                            onChange={handleChange}
+                            error={getErrorMessageAtLeast(errors.password)}
+                        />
+                        <button className='btn btn-primary w-100' type="submit" disabled={!isValid(errors)}>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginForm
