2c99f564b703c1e9aa10c52fbe57f3df6d1573a0
Dmitry <dmitry27zh@gmail.com>
2023-09-06 20:49:07 +0300

    Render form by type in Login

--- src/layout/login.jsx
+++ src/layout/login.jsx
@@ -1,20 +1,40 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
 import LoginForm from '../components/ui/loginForm'
 import { useParams } from 'react-router-dom'
+import RegisterForm from '../components/ui/registerForm'
 
 const Login = () => {
     let { type } = useParams()
     type = type === 'register' ? type : 'login'
     const [formType] = useState(type)
-    console.log(formType)
+    const renderForm = () => {
+        if (formType === 'login') {
+            return (
+                <>
+                    <LoginForm />
+                    <p>
+                        Don&apos;t have an account? <a>Sign up</a>
+                    </p>
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <RegisterForm />
+                    <p>
+                        Already have an account? <a>Sign in</a>
+                    </p>
+                </>
+            )
+        }
+    }
 
     return (
         <div className="container mt-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3 shadow p-4">
                     <h3 className="mb-4">Login</h3>
-                    <LoginForm />
+                    {renderForm()}
                 </div>
             </div>
         </div>
