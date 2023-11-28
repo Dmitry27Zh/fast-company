bb9806a29924d80a7a9b17ce716e400a8629c1d3
Dmitry <dmitry27zh@gmail.com>
2023-11-28 11:59:16 +0500

    Add http service api

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -2,3 +2,12 @@ import axios from 'axios'
 import config from '../config.json'
 
 axios.defaults.baseURL = config.apiEndpoint
+
+const httpService = {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    delete: axios.delete
+}
+
+export default httpService
