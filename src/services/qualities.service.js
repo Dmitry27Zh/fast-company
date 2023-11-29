8ddf066cb9da9469a205074b7b557a592749cea7
Dmitry <dmitry27zh@gmail.com>
2023-11-29 12:40:11 +0500

    Fix qualities service method call

--- src/services/qualities.service.js
+++ src/services/qualities.service.js
@@ -3,7 +3,7 @@ import config from '../config.json'
 
 const qualitiesService = {
     get: async () => {
-        const { data } = await httpService(config.qualitiesEndpoint)
+        const { data } = await httpService.get(config.qualitiesEndpoint)
 
         return data
     }
