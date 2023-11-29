956a6fa3bee33b7ceb5231a5190fc7947b1432d4
Dmitry <dmitry27zh@gmail.com>
2023-11-29 16:00:46 +0500

    Add global expected error interceptor in http service

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,7 +1,22 @@
 import axios from 'axios'
+import { toast } from 'react-toastify'
 import config from '../config.json'
 
 axios.defaults.baseURL = config.apiEndpoint
+
+axios.interceptors.response.use(
+    (res) => res,
+    function (error) {
+        const expectedErrors = error.response && error.response.status >= 400 && error.response.status < 500
+
+        if (!expectedErrors) {
+            console.error(error)
+            toast.error('Something was wrong. Try it later')
+        }
+
+        return Promise.reject(error)
+    }
+)
 
 const httpService = {
     get: axios.get,
