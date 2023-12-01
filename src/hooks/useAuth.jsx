40fd63b09c17b0ca8e49ee95f25acdf25fa3aaf1
Dmitry <dmitry27zh@gmail.com>
2023-12-01 13:22:06 +0500

    Set tokens

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -9,12 +9,24 @@ export const useAuth = () => {
     return useContext(AuthContext)
 }
 
+const AuthKey = {
+    TOKEN: 'jwt_token',
+    REFRESH: 'jwt_refresh_token',
+    EXPIRES: 'jwt_expires'
+}
+
 const AuthProvider = ({ children }) => {
+    function setTokens({ refreshToken, idToken, expiresIn = 3600 }) {
+        const expiresDate = new Date().getTime() + expiresIn * 1000
+        localStorage.setItem(AuthKey.TOKEN, idToken)
+        localStorage.setItem(AuthKey.REFRESH, refreshToken)
+        localStorage.setItem(AuthKey.EXPIRES, expiresDate)
+    }
     async function signUp({ email, password }) {
         const key = 'AIzaSyC5UlJfJXLHeeyYZRAL3fKf7GF4Efhf_PU'
         const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`
         const { data } = await httpAuth.post(url, { email, password, returnSecureToken: true })
-        console.log(data)
+        setTokens(data)
     }
 
     return <AuthContext.Provider value={{ signUp }}>{children}</AuthContext.Provider>
