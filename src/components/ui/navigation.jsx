6b493130d357ac0320799adc7f696deded615eb0
Dmitry <dmitry27zh@gmail.com>
2023-12-03 15:16:06 +0500

    Add auth conditional rendering in Navigation

--- src/components/ui/navigation.jsx
+++ src/components/ui/navigation.jsx
@@ -1,29 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useAuth } from '../../hooks/useAuth'
 
 const Navigation = () => {
+    const { currentUser } = useAuth()
+
     return (
-        <ul className="nav">
-            <li className="nav-item">
-                <Link className="nav-link" to="/">
+        <nav className='navbar bg-light mb-3'>
+            <div className="container-fluid">
+                <ul className="nav">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">
                     Main
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                    Login
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    className="nav-link active"
-                    to="/users"
-                    aria-current="page"
-                >
+                        </Link>
+                    </li>
+                    {currentUser && (
+                        <li className="nav-item">
+                            <Link
+                                className="nav-link active"
+                                to="/users"
+                                aria-current="page"
+                            >
                     Users
-                </Link>
-            </li>
-        </ul>
+                            </Link>
+                        </li>
+                    )}
+                </ul>
+                <div className="d-flex">
+                    {currentUser
+                        ? <p>User</p>
+                        : (
+                            <Link className="nav-link" to="/login">
+                    Login
+                            </Link>
+                        )}
+                </div>
+            </div>
+        </nav>
     )
 }
 
