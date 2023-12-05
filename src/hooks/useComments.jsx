eb3d8fb29b9bbeae352055f53c42801bc74e5577
Dmitry <dmitry27zh@gmail.com>
2023-12-05 12:20:55 +0500

    Generate comment data

--- src/hooks/useComments.jsx
+++ src/hooks/useComments.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react'
 import PropTypes from 'prop-types'
 import { useParams } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
+import { nanoid } from 'nanoid'
 
 const CommentsContext = React.createContext()
 
@@ -13,10 +15,13 @@ const CommentsProvider = ({ children }) => {
     const [isLoading, setIsLoading] = useState(true)
     const [error, setError] = useState(null)
     const { userId } = useParams()
+    const { currentUser } = useAuth()
     async function createComment(data) {
         const comment = {
             ...data,
+            _id: nanoid(),
             pageId: userId,
+            userId: currentUser._id,
             created_at: Date.now()
         }
     }
