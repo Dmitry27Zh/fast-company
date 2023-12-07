ef086992f56ad4922ce191e89299a4fb77a76e3d
Dmitry <dmitry27zh@gmail.com>
2023-12-07 14:14:13 +0500

    Redirect to personal page edit

--- src/layout/users.jsx
+++ src/layout/users.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Navigate } from 'react-router-dom'
 import UserPage from '../components/page/userPage/userPage'
 import UsersListPage from '../components/page/usersListPage'
 import EditPage from '../components/page/editPage/editPage'
 import UsersProvider from '../hooks/useUsers'
+import { useAuth } from '../hooks/useAuth'
 
 const Users = () => {
     const { userId, edit } = useParams()
+    const { currentUser } = useAuth()
     const render = () => {
         if (edit) {
-            return <EditPage id={userId} />
+            return userId === currentUser._id ? <EditPage id={userId} /> : <Navigate to={`/users/${currentUser._id}/edit`} />
         } else if (userId) {
             return <UserPage id={userId} />
         } else {
